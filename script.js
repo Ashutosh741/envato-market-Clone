ad6492d0a8d6c89ec8f4e13b29af82280abd5209
Ashutosh mishra <aashutoshmishra296@gmail.com>
2023-03-24 11:02:14 +0530

    cards_completed

--- script.js
+++ script.js
@@ -40,3 +40,42 @@ $("#owl-carousel").owlCarousel({
  nav: false,
  items: 2,
 });
+
+// count down odometer
+
+var a = 0;
+$(window).scroll(function () {
+ var oTop = $("#counter").offset().top - window.innerHeight;
+ if (a == 0 && $(window).scrollTop() > oTop) {
+  $(".counter-value").each(function () {
+   var $this = $(this),
+    countTo = $this.attr("data-count");
+   $({
+    countNum: $this.text(),
+   }).animate(
+    {
+     countNum: countTo,
+    },
+
+    {
+     duration: 2000,
+     easing: "swing",
+     step: function () {
+      $this.text(Math.floor(this.countNum));
+     },
+     complete: function () {
+      $this.text(this.countNum);
+      //alert('finished');
+     },
+    }
+   );
+  });
+  a = 1;
+ }
+});
+
+// count down random
+
+function countdown(max) {
+ return Math.floor(Math.random() * max);
+}
