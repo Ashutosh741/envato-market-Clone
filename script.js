88e6bf1765784dcdf907b0ad537465156e8adb60
Ashutosh mishra <aashutoshmishra296@gmail.com>
2023-03-26 18:21:10 +0530

    blog_post done

--- script.js
+++ script.js
@@ -79,3 +79,26 @@ $(window).scroll(function () {
 function countdown(max) {
  return Math.floor(Math.random() * max);
 }
+
+// right to left slide
+
+$(".owl-carousel").owlCarousel({
+ rtl: false,
+ loop: true,
+ margin: 10,
+ nav: true,
+ autoplay: true,
+ autoplayTimeout: 1000,
+ autoplayHoverPause: true,
+ responsive: {
+  0: {
+   items: 1,
+  },
+  600: {
+   items: 3,
+  },
+  1000: {
+   items: 5,
+  },
+ },
+});
